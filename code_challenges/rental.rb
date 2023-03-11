26851ef58179904bee246410704866e329b71f81
Scott Baitz <Scott.W.Baitz@gmail.com>
2023-03-11 12:31:02 -0700

    Add a completed rental challenge

--- code_challenges/rental.rb
+++ code_challenges/rental.rb
@@ -12,3 +12,13 @@
 # - If the user either has a good record or is over 25 years old, they should pay full price
 # - If the user is not over 25 and has a bad driving record, they need to have someone else sign for the rental
 
+good_driving_record = true
+age = 25
+
+if good_driving_record == true && age > 25
+    puts "You get a discounted rate on your rental car"
+elsif good_driving_record == true || age > 25
+    puts "You may rent the car for full price"
+else
+    puts "Someone else must sign for the rental"
+end
