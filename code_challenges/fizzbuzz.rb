4d215b9aef1c8fbe89d67c00a55d9f96c5c99b3e
Scott Baitz <Scott.W.Baitz@gmail.com>
2023-03-11 13:11:08 -0700

    Remove a method to allow it to function with floats

--- code_challenges/fizzbuzz.rb
+++ code_challenges/fizzbuzz.rb
@@ -2,15 +2,24 @@
 
 # Write a Ruby program that prints out a String or Integer: The printed value will depend on the value of an Integer. If the Integer is a multiple of 3, print "Fizz". If the Integer is a multiple of 5, print "Buzz". If the Integer is a multiple of both 3 and 5, print "FizzBuzz". If the Integer is not a multiple of either, print the Integer itself.
 
-integer = 11
+integer = 15.2
 
-if integer.gcd(5) == 5 && integer.gcd(3) == 3
+# if integer.gcd(5) == 5 && integer.gcd(3) == 3
+#     puts "FizzBuzz"
+# elsif integer.gcd(5) == 5
+#     puts "Buzz"
+# elsif integer.gcd(3) == 3
+#     puts "Fizz"
+# else
+#     puts integer
+# end
+
+if integer % 5 == 0 && integer % 3 == 0
     puts "FizzBuzz"
-elsif integer.gcd(5) == 5
+elsif integer % 5 == 0
     puts "Buzz"
-elsif integer.gcd(3) == 3
+elsif integer % 3 == 0
     puts "Fizz"
 else
-    puts "#{integer}"
+    puts integer
 end
-
